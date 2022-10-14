e495f101fd542c19aa983edd4e1e1c251efd3a62
David Fragua <davidfragua@gmail.com>
2022-10-14 18:41:46 +0200

    list of movies added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Movies = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+router.get("/movies", (req, res, next) => {
+    Movies.find()
+    .then((response) =>{
+        console.log(response);
+        res.render("movies.hbs", {
+        moviesList: response
+        })
+    })
+    .catch((err) => {
+        next(err)
+    })
+})
+
+
 
 module.exports = router;
